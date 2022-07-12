b4ff4a09364c3b18950ab91d5ec674a4310f8d40
Ilia Talebzadeh <iliasins82@gmail.com>
2022-07-13 00:30:14 +0430

    created new relations between users and posts tables

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   validates :title, length: { minimum: 6, maximum: 100 }, presence: true
   validates :body, length: { minimum: 200 }, presence: true
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_161428) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_193355) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
